fc4613733682c22448fd999c20ad67da4b5485df
ljh255 <jinhai@carbonstop.net>
2024-07-26 16:24:18 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,18 @@ npm run build
 # cd 到构建输出的目录下
 cd src/.vuepress/dist
 # # 初始化git
-# git init
+git init
 
 # # 配置git用户
 # git config user.name "brian"
 # git config user.email "brian@toimc.com"
 
-# git add -A
-# git commit -m 'update commit'
+git add -A
+git commit -m 'update commit'
 
 # # 部署到 github
 # git remote add github git@toimc:toimc/notes-page.git
-# git push -f github master
+git push -f git@github.com:lijinhai255/lijinhai255.github.io.git
 
 # git remote add gitee git@gitee.com:toimc/notes-page.git
 # git push -f gitee master
